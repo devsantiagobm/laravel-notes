28d8571c3c1873315bc1a45b2d921a8b82b50cc0
Sebastian <sanseb290514@gmail.com>
2023-08-02 19:50:01 -0500

    Force redirect http to https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,24 +1,23 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
-{
+class AppServiceProvider extends ServiceProvider {
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
+    public function register(): void {
         //
     }
 
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        //
+    public function boot(): void {
+        if (env('APP_ENV') == 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
